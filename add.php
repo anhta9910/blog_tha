70ea6e62cf9f99d602eaa28e77de85109ec42f7f
Ta Hong Anh <Ta Anh>
2020-07-04 00:47:42 +0700

    add add.php + ckeditor

---
+++ add.php
@@ -0,0 +1,35 @@
+<?php 
+    include('header.php')
+?>
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Add Post</div>
+                <div class="card-body">
+                    <div class="form-group">
+                        <label for="title">Title</label>
+                        <input type="text" name = "title" class="form-control">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="description">Description</label>
+                        <textarea name="description" id="editor" cols="10" class="form-control"></textarea>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<script>
+    ClassicEditor
+        .create( document.querySelector( '#editor' ) )
+        .catch( error => {
+            console.error( error );
+        } );
+</script>
+<style type="text/css">
+    .card{
+        margin-top:10px;
+    }
+</style>
